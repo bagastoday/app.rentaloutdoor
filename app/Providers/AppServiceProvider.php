ffd436b5b29b9367f15f909cc0147a5ed69082ad
bagastoday <muhamad.bagas5135@smk.belajar.id>
2026-08-27 15:07:16 +0700

    Fix asset URL routing for Vercel deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production') || env('APP_ENV') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
+
+        if (env('ASSET_URL') === null) {
+            config(['app.asset_url' => '/']);
+        }
     }
 }
